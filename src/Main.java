524cc6f51ea9b8e6ef81ad2ba1af1b2163012eee
User <333@fd.com>
2022-11-14 22:50:42 +0400

    Домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Man maksim = new Man("   ", "Минск", -1998," ");
+        Man anna = new Man("Аня","Москва",1993,"методист образовательных программ");
+        Man katia = new Man("Катя", "Калининград", 1992,"продакт-менеджер");
+        Man artem = new Man("Артём", "Москва", 1995,"директор по развитию бизнеса");
+        maksim.printInfo();
+        katia.printInfo();
+        artem.printInfo();
+        anna.printInfo();
+
     }
 }
